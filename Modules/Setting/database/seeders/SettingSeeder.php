a40ed6a051578646f64e9a0eaf0eb6d6190c1cb3
a-nemr <ahmed-elnemr@moltaqa.net>
2026-05-17 16:28:09 +0300

    SEEDER SETTING

--- Modules/Setting/database/seeders/SettingSeeder.php
+++ Modules/Setting/database/seeders/SettingSeeder.php
@@ -3,8 +3,6 @@
 namespace Modules\Setting\database\seeders;
 
 use Illuminate\Database\Seeder;
-use Modules\Setting\app\Models\Setting;
-use App\SettingTypeEnum;
 
 class SettingSeeder extends Seeder
 {
@@ -13,32 +11,6 @@ class SettingSeeder extends Seeder
      */
     public function run(): void
     {
-        if (Setting::count() === 0) {
-            Setting::create([
-                'name' => ['en' => 'Site Name', 'ar' => 'اسم الموقع'],
-                'key' => 'site_name',
-                'value' => ['en' => 'Purple Agency', 'ar' => 'وكالة الأرجواني'],
-                'type' => SettingTypeEnum::SHORT_TEXT,
-                'is_translatable' => true,
-            ]);
-
-            Setting::create([
-                'name' => ['en' => 'Contact Email', 'ar' => 'البريد الإلكتروني للتواصل'],
-                'key' => 'contact_email',
-                'value' => 'info@purple.com',
-                'type' => SettingTypeEnum::SHORT_TEXT,
-                'is_translatable' => false,
-            ]);
-
-            Setting::create([
-                'name' => ['en' => 'Phone Number', 'ar' => 'رقم الهاتف'],
-                'key' => 'phone_number',
-                'value' => '+963 11 222 3333',
-                'type' => SettingTypeEnum::SHORT_TEXT,
-                'is_translatable' => false,
-            ]);
-        }
-
         if (\Modules\Setting\app\Models\GeneralSetting::count() === 0) {
             \Modules\Setting\app\Models\GeneralSetting::create([
                 'address' => [
