9d4a88ea308b2287105ffba8397ec44682d0bb41
a-nemr <ahmed-elnemr@moltaqa.net>
2026-05-18 16:50:15 +0300

    fix(filesystem) web

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,26 @@ Route::get('/', function () {
     return redirect('admin');
 });
 Route::post('/support/messages', [HomeController::class, 'submitSupport'])->name('support.message.store');
+
+Route::get('/run-link', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('storage:link');
+
+        return 'Storage link created successfully!';
+    } catch (\Exception $e) {
+        return 'Error: '.$e->getMessage();
+    }
+});
+
+Route::get('/run-clear', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('config:clear');
+        \Illuminate\Support\Facades\Artisan::call('cache:clear');
+        \Illuminate\Support\Facades\Artisan::call('route:clear');
+        \Illuminate\Support\Facades\Artisan::call('view:clear');
+
+        return 'Cache, Config, Routes, and Views cleared successfully!';
+    } catch (\Exception $e) {
+        return 'Error: '.$e->getMessage();
+    }
+});
