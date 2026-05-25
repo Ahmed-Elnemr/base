f0bd2b35274f2a11cfe646dbdfc17ac2d424963b
a-nemr <ahmed-elnemr@moltaqa.net>
2026-05-25 17:17:04 +0300

    fix(service)add related_works

--- Modules/Service/database/migrations/2026_05_25_115441_add_related_works_to_services_table.php
+++ Modules/Service/database/migrations/2026_05_25_115441_add_related_works_to_services_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('services', function (Blueprint $table) {
-            $table->json('related_works')->nullable();
+            $table->longText('related_works')->nullable();
         });
     }
 
